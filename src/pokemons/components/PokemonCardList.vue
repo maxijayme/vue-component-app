2a9a6c74cac05e852e32698ba92d85bd81aee9f2
maxijayme <maxijayme@gmail.com>
2024-12-08 12:32:14 +0100

    pokemon list and cards

---
+++ src/pokemons/components/PokemonCardList.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import type { Pokemon } from '../interfaces';
+import PokemonCard from './PokemonCard.vue';
+
+  interface Props {
+    pokemons: Pokemon[]
+  }
+  defineProps<Props>()
+</script>
+
+<template>
+   <div class="card-list">
+     <PokemonCard v-for="(pokemon, index) in pokemons" :key="index" :pokemon/>
+   </div>
+</template>
+
+<style scoped>
+  .card-list {
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+  }
+</style>
--- src/pokemons/pages/PokemonList.vue
+++ src/pokemons/pages/PokemonList.vue
@@ -1,26 +1,22 @@
 <script setup lang="ts">
 import { usePokemons } from '../composables/usePokemons';
+import PokemonCardList from '@/pokemons/components/PokemonCardList.vue';
 
 const props = defineProps<{
   title: string
 }>()
 
-const { pokemons, isLoading, count } = usePokemons()
+const { pokemons, isLoading, count, error, isError } = usePokemons()
 
 
 </script>
 
 <template>
-  <div>
+  <h1 v-if="isLoading">Loading...</h1>
+  <div v-else-if="isError">{{ error }}</div>
+  <div v-else>
     <h1>{{ props.title }}</h1>
-    <h5>{{ count }} en total</h5>
+    <h3>{{ count }} pokemons</h3>
   </div>
-  <div>
-    <span v-if="isLoading">Loading...</span>
-    <ul>
-      <li v-for="pokemon in pokemons" :key="pokemon.id">
-        {{ pokemon.name }}
-      </li>
-    </ul>
-  </div>
+  <PokemonCardList :pokemons="pokemons ?? []" />
 </template>
